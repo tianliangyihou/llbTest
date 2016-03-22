54a0b7df0b83fe21a2ab27ac41edb92e31e058e1
1 <a1@llb.local>
2016-03-22 19:38:51 +0800

    Initial Commit

---
+++ llbTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  llbTest
+//
+//  Created by 1 on 16/3/22.
+//  Copyright © 2016年 llb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
